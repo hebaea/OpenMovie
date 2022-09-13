85a4f12c26be5ed923540a6178e830bba85a71f6
ABD ULMAGID ULMAGHBUB <47678429+Zeglaty@users.noreply.github.com>
2022-09-13 11:46:59 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:lib/app/modules/onboarding/views/onboarding_view.dart

--- lib/app/modules/onboarding/views/onboarding_view.dart
+++ lib/app/modules/onboarding/views/onboarding_view.dart
@@ -22,14 +22,68 @@ class OnboardingView extends GetView<OnboardingController> {
           Align(
             alignment: FractionalOffset.bottomCenter,
             child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.amber,
+              decoration: const BoxDecoration(
+                  color: AppColors.layerOnePrimary,
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(20),
                       topRight: Radius.circular(20))),
               height: 250,
               width: double.infinity,
-              // add text and button
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 63),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(height: 37),
+                      const Center(
+                        child: Text(
+                          'Keep Trak',
+                          style: TextStyle(
+                              color: AppColors.textAppPrimay,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Center(
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          'the marvie move univers is one step away from you, stay tuned and give your apenionstay tuned and.',
+                          style: TextStyle(
+                            color: AppColors.textWight,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 21),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: AppColors.buttonPrimary,
+                            side: BorderSide(
+                              width: 2.0,
+                              color: AppColors.buttonPrimaryBorder,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(8), // <-- Radius
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'Next',
+                            style: TextStyle(
+                              color: AppColors.textWight,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      )
+                    ]),
+              ),
             ),
           ),
         ],
--- lib/colors/app_colors.dart
+++ lib/colors/app_colors.dart
@@ -3,21 +3,21 @@ import 'package:flutter/scheduler.dart';
 
 class AppColors {
   //General
-  static const Color primary = Color(0xF9C22E);
-  static Color primary20 = Color(0xF9C22E).withOpacity(0.2);
+  static const Color primary = Color(0xffF9C22E);
+  static Color primary20 = Color(0xff44391C);
 
   //Layers
-  static const Color background = Color(0x0B0B0B);
-  static const Color layerOnePrimary = Color(0x171717);
+  static const Color background = Color(0xff0B0B0B);
+  static const Color layerOnePrimary = Color(0xff171717);
 
   //Text
-  static const Color textPrimay = Color(0xF9C22E);
-  static const Color textWight = Color(0xFFFFFF);
-  static const Color textLightGray = Color(0x4E4E4E);
+  static const Color textPrimay = Color(0xffF9C22E);
+  static const Color textWight = Color(0xffFFFFFF);
+  static const Color textLightGray = Color(0xff4E4E4E);
 
   //Buttons
-  static const Color buttonPrimary = Color(0xF9C22E);
-  static Color buttonPrimaryBorder = Color(0xF9C22E).withOpacity(0.2);
+  static const Color buttonPrimary = Color(0xffF9C22E);
+  static Color buttonPrimaryBorder = Color(0xff44391C);
 
   // bool isDarkMode() {
   //   // mode
